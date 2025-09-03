75dd035c8bc13e08333ba2a56c5471a30f64b437
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2025-09-02 21:35:25 -0700

    feat(App component) : Component updated Updated component with new functions to handle the submit and handle events to add the tasks the user has selected to add on the to do list

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [tasks, setTasks] = useState([]);
+  const [userTask, setUsertask] = useState('');
+  const [errorMessage, setErrormessage] = useState('')
+
+  const getUserTask = (e)=>{
+
+    let newTask = e.target.value;
+    setUsertask(newTask)
+
+  }
+
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    
+
+    if(userTask === ''){
+      setErrormessage('Add a valid task!')
+      return;
+    }
+
+    setTasks([...tasks, userTask])
+    setErrormessage('');
+    setUsertask('');
+  }
+
+  const removeTask = (index)=>{
+    setTasks((tasks.filter((_, i) =>  i !== index )));
+  }
+
+  console.log(tasks);
+
+  return (
+    <div className='mainContainer'>
+      <main>
+        <form onSubmit={handleSubmit} action="">
+          <input value={userTask} name='taskInput' type="text" onChange={getUserTask}/>
+          <button type='submit'>+</button>
+        </form>
+        <p>{errorMessage}</p>
+        <div>
+          <ul>
+            {
+              tasks.map((task, index)=>{
+                return(
+                  <li key={index}>{task}<button onClick={()=>removeTask(index)}>-</button></li>
+                )
+              })
+            }
+          </ul>
+        </div>
+        </main>
+    </div>
+
+  )
+}
+
+export default App
